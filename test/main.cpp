812a42e46e62597460a9c76d72a5fe61bebb48c8
Penggf <jxpenggf@gmail.com>
2021-01-07 14:12:34 +0800

    version for test

---
+++ test/main.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+#include <vector>
+using namespace std;
+#include <ctime>
+#include <queue>
+#include<string>
+#include <sstream>
+#include <boost/shared_ptr.hpp>
+
+struct Point
+{
+    Point(float x_, float y_, float z_):x(x_),y(y_),z(z_){}
+    float x, y, z;
+};
+int main (int argc, char** argv)
+
+
+
+
+}
